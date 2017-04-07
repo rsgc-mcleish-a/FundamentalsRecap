d700b31b0f00d343566668164884f197faac3931
Student <student@mac-book-angus-mcleish.local>
2017-04-06 21:35:24 -0400

    sdfs

--- Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -27,6 +27,8 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 400, height: 400)
 
+canvas.drawShapesWithBorders = false
+
 //Brown Color
 canvas.fillColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
 
@@ -65,10 +67,10 @@ for x in stride(from: 25, through: 450, by: 70){
     canvas.drawEllipse(centreX: x, centreY: 35, width: 45, height: 45)
 }
 
-//Orange Color
+//Yellow Color
 canvas.fillColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
 
-//Orange Circles
+//Yellow Circles
 for x in stride(from: 25, through: 450, by: 70){
     
     //First Row
@@ -105,6 +107,47 @@ for x in stride(from: 25, through: 450, by: 70){
 
 
 
+
+
+
+
+//Yellow Color
+canvas.fillColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
+
+//Yellow Circles
+for x in stride(from: 60, through: 450, by: 70){
+    
+    //First Row
+    canvas.drawEllipse(centreX: x, centreY: 340, width: 55, height: 55)
+    
+    //Sedond Row
+    canvas.drawEllipse(centreX: x, centreY: 275, width: 55, height: 55)
+    
+    //Third Row
+    canvas.drawEllipse(centreX: x, centreY: 150, width: 55, height: 55)
+    
+    //Forth Row
+    canvas.drawEllipse(centreX: x, centreY: 60, width: 55, height: 55)
+}
+
+//Orange Color
+canvas.fillColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
+
+//Orange Circles
+for x in stride(from: 60, through: 450, by: 70){
+    
+    //First Row
+    canvas.drawEllipse(centreX: x, centreY: 340, width: 40, height: 40)
+    
+    //Sedond Row
+    canvas.drawEllipse(centreX: x, centreY: 275, width: 40, height: 40)
+    
+    //Third Row
+    canvas.drawEllipse(centreX: x, centreY: 150, width: 40, height: 40)
+    
+    //Forth Row
+    canvas.drawEllipse(centreX: x, centreY: 60, width: 40, height: 40)
+}
 
 
 
@@ -115,74 +158,51 @@ canvas.fillColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 10
 for x in stride(from: 60, through: 450, by: 70){
     
     //First Row
-    canvas.drawEllipse(centreX: x, centreY: 300, width: 60, height: 60)
+    canvas.drawEllipse(centreX: x, centreY: 340, width: 25, height: 25)
     
     //Second Row
-    canvas.drawEllipse(centreX: x, centreY: 250, width: 60, height: 60)
+    canvas.drawEllipse(centreX: x, centreY: 275, width: 25, height: 25)
     
     //Third Row
-    canvas.drawEllipse(centreX: x, centreY: 175, width: 60, height: 60)
+    canvas.drawEllipse(centreX: x, centreY: 150, width: 25, height: 25)
     
     //Fourth Row
-    canvas.drawEllipse(centreX: x, centreY: 35, width: 60, height: 60)
+    canvas.drawEllipse(centreX: x, centreY: 60, width: 25, height: 25)
 }
 
-//Orange Color
-canvas.fillColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
 
-//Orange Circles
+
+
+//White lines
+for x in stride(from: 60, through: 450, by: 70){
+    
+canvas.lineColor = Color.white
+
+canvas.defaultLineWidth = 15
+
+//First row
+canvas.drawLine(fromX: x, fromY: 345, toX: x, toY: 450)
+    
+//Second row
+canvas.drawLine(fromX: x, fromY: 155, toX: x, toY: 270)
+
+//Third row
+canvas.drawLine(fromX: x, fromY: 0, toX: x, toY: 57)
+}
+
+
+//White lines
 for x in stride(from: 25, through: 450, by: 70){
+
+canvas.defaultLineWidth = 17
     
-    //First Row
-    canvas.drawEllipse(centreX: x, centreY: 365, width: 45, height: 45)
+//First row
+canvas.drawLine(fromX: x, fromY: 255, toX: x, toY: 360)
     
-    //Sedond Row
-    canvas.drawEllipse(centreX: x, centreY: 250, width: 45, height: 45)
-    
-    //Third Row
-    canvas.drawEllipse(centreX: x, centreY: 175, width: 45, height: 45)
-    
-    //Forth Row
-    canvas.drawEllipse(centreX: x, centreY: 35, width: 45, height: 45)
+//Second row
+canvas.drawLine(fromX: x, fromY: 0, toX: x, toY: 30)
 }
 
-//Orange Color
-canvas.fillColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
-
-//Orange Circles
-for x in stride(from: 25, through: 450, by: 70){
-    
-    //First Row
-    canvas.drawEllipse(centreX: x, centreY: 365, width: 30, height: 30)
-    
-    //Sedond Row
-    canvas.drawEllipse(centreX: x, centreY: 250, width: 30, height: 30)
-    
-    //Third Row
-    canvas.drawEllipse(centreX: x, centreY: 175, width: 30, height: 30)
-    
-    //Forth Row
-    canvas.drawEllipse(centreX: x, centreY: 35, width: 30, height: 30)
-}
-
-//White Color
-canvas.fillColor = Color.white
-
-//White Circles
-for x in stride(from: 25, through: 450, by: 70){
-    
-    //First Row
-    canvas.drawEllipse(centreX: x, centreY: 365, width: 15, height: 15)
-    
-    //Sedond Row
-    canvas.drawEllipse(centreX: x, centreY: 250, width: 15, height: 15)
-    
-    //Third Row
-    canvas.drawEllipse(centreX: x, centreY: 175, width: 15, height: 15)
-    
-    //Forth Row
-    canvas.drawEllipse(centreX: x, centreY: 35, width: 15, height: 15)
-}
 
 
 
